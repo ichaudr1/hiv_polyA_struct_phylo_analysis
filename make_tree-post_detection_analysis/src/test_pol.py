95a2a3342e374aa0840496b3aed5add79a6fb3d1
Issac Chaudry <issac@bright72.cm.cluster>
2024-03-03 00:06:09 -0500

    .

--- make_tree-post_detection_analysis/src/test_pol.py
+++
@@ -1,5 +0,0 @@
-from make_tree import fetch_phylo
-
-accession = 'AF110978'
-
-print(fetch_phylo(accession))
--- polyA_detection/src/gag_check.py
+++
@@ -1,8 +0,0 @@
-'''
-testing the fetch gag function
-'''
-
-from polyA_parsing import gag_position
-
-accession = 'AF179368'
-print(gag_position(accession))
